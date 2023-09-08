3c0683cf08b9015a7cc967623b971042c8d5f75c
Shaiju Rajan <shaijuvr@rediffmail.com>
2023-09-08 20:43:52 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp=require('gulp');
 var del=require('del');
+var del2=require('del');
+
 
 gulp.task('cleanBuild',function(done){
     del(['app/**/*.js','app.services/**/*.js','mocks/**/*.js']);
